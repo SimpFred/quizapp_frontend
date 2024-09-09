2381b1310924f2add5719db3951add72a15ed522
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-09-09 14:52:06 +0200

    Refactor Result component to display correct/incorrect indicator icons next to the quiz results

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import { Box, Typography, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 function Result({ answers, questions, correctAnswers, incorrectAnswers }) {
   return (
     <Box sx={{ width: '100%' }}>
       {questions.map((question, index) => (
-        <Accordion key={index}>
+        <Accordion key={index} sx={{ marginBottom: 1}}>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
             aria-controls={`panel${index}-content`}
             id={`panel${index}-header`}
+            sx={{ paddingLeft: 1 }}
           >
+            <Box sx={{ display: 'flex', alignItems: 'center', marginRight: 1 }}>
+              {answers[index] === question.correct_answer ? (
+                <CheckCircleIcon sx={{ color: 'success.main' }} />
+              ) : (
+                <CancelIcon sx={{ color: 'error.main' }} />
+              )}
+            </Box>
             <Typography
               variant="body1"
               dangerouslySetInnerHTML={{ __html: question.question }}
-              sx={{ marginRight: 2 }} // Lägg till marginal till höger
+              sx={{ marginRight: 2 }}
             />
           </AccordionSummary>
           <AccordionDetails>
-          <Typography variant="body2" color="textSecondary">
+            <Typography variant="body2" color="textSecondary">
               Correct answer: {question.correct_answer}
             </Typography>
             <Typography variant="body2" color="textSecondary">
