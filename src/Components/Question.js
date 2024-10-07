e1619f90db860694077308364861a9d54b7f506c
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-10-07 14:05:27 +0200

    Refactor Question component to use QuizAppContext

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Grid, Typography, Button } from '@mui/material';
+import React, { useState, useEffect, useContext } from "react";
+import { Grid, Typography, Button } from "@mui/material";
+import { QuizAppContext } from "../context";
 
-function Question({ question, onAnswer }) {
+function Question() {
+  const { currentQuestion, onAnswer } = useContext(QuizAppContext);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [shuffledAnswers, setShuffledAnswers] = useState([]);
 
@@ -22,34 +24,44 @@ function Question({ question, onAnswer }) {
 
   // Shuffle the answer options when the component mounts or the question changes
   useEffect(() => {
-    if (question) {
-      const answers = [...question.incorrect_answers, question.correct_answer];
+    if (currentQuestion) {
+      const answers = [
+        ...currentQuestion.incorrect_answers,
+        currentQuestion.correct_answer,
+      ];
       setShuffledAnswers(shuffleArray(answers));
     }
-  }, [question]);
+  }, [currentQuestion]);
 
   // If no question is available, display a loading message
-  if (!question) {
+  if (!currentQuestion) {
     return <Typography variant="h6">Loading question...</Typography>;
   }
 
   // Render the question and answer options
   return (
     <div>
-      <Typography p={2} textAlign="center" variant="h5" gutterBottom dangerouslySetInnerHTML={{ __html: question.question }}></Typography>
+      <Typography
+        p={2}
+        textAlign="center"
+        variant="h5"
+        gutterBottom
+        dangerouslySetInnerHTML={{ __html: currentQuestion.question }}
+      ></Typography>
       <Grid container spacing={3}>
         {shuffledAnswers.map((answer, index) => (
           <Grid item xs={6} key={index}>
-            
-              <Button
-                variant="outlined"
-                color="primary"
-                fullWidth
-                onClick={() => handleAnswer(answer)}
-                style={{ backgroundColor: selectedAnswer === answer ? 'lightblue' : '' }}
-              >
-                <Typography dangerouslySetInnerHTML={{ __html: answer }} />
-              </Button>
+            <Button
+              variant="outlined"
+              color="primary"
+              fullWidth
+              onClick={() => handleAnswer(answer)}
+              style={{
+                backgroundColor: selectedAnswer === answer ? "lightblue" : "",
+              }}
+            >
+              <Typography dangerouslySetInnerHTML={{ __html: answer }} />
+            </Button>
           </Grid>
         ))}
       </Grid>
