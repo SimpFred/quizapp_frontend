8e8a44108760884ec2f6a07bff5b7123aa0f70db
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-10-09 13:53:38 +0200

    Added comments

--- src/Components/helperComponents/Error.js
+++ src/Components/helperComponents/Error.js
@@ -2,6 +2,24 @@ import React, { useContext } from "react";
 import { Container, Typography } from "@mui/material";
 import { QuizAppContext } from "../../context";
 
+/**
+ * Error Component
+ *
+ * This component displays an error message retrieved from the QuizAppContext.
+ * It uses Material-UI's Container and Typography components to style the error message.
+ *
+ * @component
+ * @example
+ * // To use the Error component, ensure that it is wrapped within the QuizAppProvider
+ * // and that the QuizAppContext contains an error message.
+ * return (
+ *   <QuizAppProvider>
+ *     <Error />
+ *   </QuizAppProvider>
+ * )
+ *
+ * @returns {JSX.Element} A container with an error message.
+ */
 function Error() {
   const { message } = useContext(QuizAppContext);
   return (
