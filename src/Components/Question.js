e3033943b473676faa419e2faed3e92a521b621c
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-10-09 14:14:05 +0200

    fix: Update Grid component usage to avoid deprecation warnings
    - Replaced deprecated Grid component with Grid2

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,20 +1,50 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Grid, Typography, Button } from "@mui/material";
+import { Grid2, Typography, Button } from "@mui/material";
 import { QuizAppContext } from "../context";
 import parse from "html-react-parser";
 
+/**
+ * Question Component
+ *
+ * This component displays the current quiz question and its possible answers.
+ * It uses Material-UI components for styling and layout. The question and answers
+ * are fetched from the QuizAppContext. The answers are shuffled and displayed as buttons.
+ * When an answer is selected, it is highlighted and the onAnswer function from the context is called.
+ *
+ * @component
+ * @example
+ * // To use the Question component, ensure that it is wrapped within the QuizAppProvider
+ * // and that the QuizAppContext contains the necessary state and functions.
+ * return (
+ *   <QuizAppProvider>
+ *     <Question />
+ *   </QuizAppProvider>
+ * )
+ *
+ * @returns {JSX.Element} A container with the current quiz question and possible answers.
+ */
 function Question() {
   const { currentQuestion, onAnswer } = useContext(QuizAppContext);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [shuffledAnswers, setShuffledAnswers] = useState([]);
 
-  // Handles when the user selects an answer
+  /**
+   * Handles the selection of an answer.
+   * Sets the selected answer and calls the onAnswer function from the context.
+   *
+   * @param {string} answer - The selected answer
+   */
   const handleAnswer = (answer) => {
     setSelectedAnswer(answer);
     onAnswer(answer);
   };
 
-  // Function to shuffle an array
+  /**
+   * Shuffles an array.
+   *
+   * @param {Array} array - The array to shuffle
+   * @returns {Array} The shuffled array
+   */
   const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -23,7 +53,9 @@ function Question() {
     return array;
   };
 
-  // Shuffle the answer options when the component mounts or the question changes
+  /**
+   * useEffect hook to shuffle the answers when the current question changes.
+   */
   useEffect(() => {
     if (currentQuestion) {
       const answers = [
@@ -34,20 +66,18 @@ function Question() {
     }
   }, [currentQuestion]);
 
-  // If no question is available, display a loading message
   if (!currentQuestion) {
     return <Typography variant="h6">Loading question...</Typography>;
   }
 
-  // Render the question and answer options
   return (
     <div>
       <Typography p={2} textAlign="center" variant="h5" gutterBottom>
         {parse(currentQuestion.question)}
       </Typography>
-      <Grid container spacing={3}>
+      <Grid2 container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         {shuffledAnswers.map((answer, index) => (
-          <Grid item xs={6} key={index}>
+          <Grid2 size={6} key={index}>
             <Button
               variant="outlined"
               color="primary"
@@ -59,9 +89,9 @@ function Question() {
             >
               <Typography>{parse(answer)}</Typography>
             </Button>
-          </Grid>
+          </Grid2>
         ))}
-      </Grid>
+      </Grid2>
     </div>
   );
 }
