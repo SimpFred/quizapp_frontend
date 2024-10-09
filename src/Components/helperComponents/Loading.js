6145b866b47a9df8b1498b4b87e5d729845ce8f0
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-10-09 13:54:24 +0200

    Refactor Loading component to improve code readability by adding documentation

--- src/Components/helperComponents/Loading.js
+++ src/Components/helperComponents/Loading.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import { Container, Typography, CircularProgress, Box } from '@mui/material';
+import React from "react";
+import { Container, Typography, CircularProgress, Box } from "@mui/material";
 
+/**
+ * Loading Component
+ *
+ * This component displays a loading spinner and a customizable loading message.
+ * It uses Material-UI's Container, Typography, CircularProgress, and Box components
+ * to style and position the loading elements.
+ *
+ * @component
+ * @example
+ * // To use the Loading component, simply include it in your render method.
+ * // You can optionally pass a custom loading message.
+ * return (
+ *   <Loading loadingText="Fetching data..." />
+ * )
+ *
+ * @param {Object} props - The component props
+ * @param {string} [props.loadingText="Loading questions..."] - The loading message to display
+ * @returns {JSX.Element} A container with a loading spinner and message.
+ */
 function Loading({ loadingText = "Loading questions..." }) {
   return (
     <Container maxWidth="sm">
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100vh',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
         }}
       >
-        <CircularProgress 
+        <CircularProgress
           sx={{
-            animationDuration: '3s', // Justerar hastigheten på snurrningen
+            animationDuration: "3s", // Justerar hastigheten på snurrningen
           }}
         />
         <Typography variant="h6" sx={{ mt: 2 }}>
