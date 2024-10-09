2034a9fa018668f63a7df062acb4d4434a33f260
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-10-09 14:20:27 +0200

    Refactor QuizAppProvider component to improve code readability by adding documentation and comments

--- src/context.js
+++ src/context.js
@@ -1,7 +1,33 @@
 import { createContext, useState } from "react";
 
+/**
+ * QuizAppContext
+ *
+ * This context provides the state and functions for managing the quiz application.
+ * It includes state for questions, answers, quiz setup, and various flags for handling
+ * the quiz flow.
+ */
 export const QuizAppContext = createContext();
 
+/**
+ * QuizAppProvider Component
+ *
+ * This component provides the QuizAppContext to its children. It manages the state
+ * and functions required for the quiz application.
+ *
+ * @component
+ * @example
+ * // To use the QuizAppProvider, wrap it around your component tree.
+ * return (
+ *   <QuizAppProvider>
+ *     <YourComponent />
+ *   </QuizAppProvider>
+ * )
+ *
+ * @param {Object} props - The props for the component.
+ * @param {React.ReactNode} props.children - The child components that will have access to the context.
+ * @returns {JSX.Element} The QuizAppProvider component with context values.
+ */
 export const QuizAppProvider = ({ children }) => {
   const [questions, setQuestions] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -17,6 +43,12 @@ export const QuizAppProvider = ({ children }) => {
   const [updateScoreboard, setUpdateScoreboard] = useState(false);
   const [hasSaveDialogBeenOpened, setHasSaveDialogBeenOpened] = useState(false);
 
+  /**
+   * Handles the user's answer to a question.
+   * Updates the correct and incorrect answers count, and moves to the next question.
+   *
+   * @param {string} answer - The user's selected answer.
+   */
   const onAnswer = (answer) => {
     const currentQuestion = questions[currentQuestionIndex];
     if (answer === currentQuestion.correct_answer) {
@@ -34,14 +66,26 @@ export const QuizAppProvider = ({ children }) => {
     }
   };
 
+  /**
+   * Handles the start of the quiz by setting up the quiz configuration.
+   *
+   * @param {Object} setup - The quiz setup configuration.
+   */
   const handleStartQuiz = (setup) => {
     setQuizSetup(setup);
   };
 
+  /**
+   * Closes the save dialog.
+   */
   const handleCloseSaveDialog = () => {
     setOpenSaveDialog(false);
   };
 
+  /**
+   * Handles the event when the quiz result is saved.
+   * Updates the state to reflect that the result has been saved.
+   */
   const handleResultSaved = () => {
     setHasSaveDialogBeenOpened(true);
     setUpdateScoreboard(true);
